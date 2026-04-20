f0db9d20abe172f54df3fd4f693582fb7c1a9dba
whiplashFire <3232069085@qq.com>
2026-04-20 17:31:25 +0800

    Update vite.config.js
    我要累死了

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/ych-checker/',
   plugins: [react(), tailwindcss()],
 })
